644c03cdb1c5d3ab8d0b36cf2f7b52f7ce144352
Mathieu Desvé <mathieu@ogury.co>
2016-10-29 15:07:27 +0200

    Add dynamic variable replacement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,10 @@
 node_modules/.bin/cfn-lambda zip --output deploy/archive.zip
 
 echo "Deploy $TRAVIS_TAG version to S3"
-aws s3 cp deploy/archive.zip s3://chatanoo-deployment/aws-cloudformation-s3-uploader/$TRAVIS_TAG.zip
+aws s3 cp deploy/archive.zip s3://chatanoo-deployment.eu-west-1/aws-cloudformation-s3-uploader/$TRAVIS_TAG.zip
 
 echo "Upload latest"
 aws s3api put-object \
-  --bucket chatanoo-deployment \
+  --bucket chatanoo-deployment.eu-west-1 \
   --key aws-cloudformation-s3-uploader/latest.zip \
-  --website-redirect-location /chatanoo-deployment/aws-cloudformation-s3-uploader/$TRAVIS_TAG.zip
+  --website-redirect-location /chatanoo-deployment.eu-west-1/aws-cloudformation-s3-uploader/$TRAVIS_TAG.zip
